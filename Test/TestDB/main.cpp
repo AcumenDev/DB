4b257ad6ce7dc31561f34555efd7ad486a928fef
Vladimir Stalmakov <akummail@gmail.com>
2013-06-08 17:22:12 +0400

    fix test project

--- Test/TestDB/main.cpp
+++ Test/TestDB/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
-#include "../../src/Connection/DefDB.h"
-#include "../../src/Connection/DBConnection.h"
+
+#include "Connection/DBConnection.h"
+
 //#include "../../src/CodeGeneration/Cpp/CGCpp.h"
 
 using namespace std;
-using namespace Connection;
-using namespace DB;
+//using namespace Connection;
+//using namespace DB;
 int main()
 {
     cout << "Hello world!" << endl;
-    std::shared_ptr<DBBase> dbSqlLite = DBConnection::GetConnection(DBType::Sqllite);
+
+   //  Connection::DBConnection var;
+
+    // LoggingSystem * Log = LoggingSystem::GetLoggingSystem();
+
+//   Log->Write("Hello world!");
+
+   // std::shared_ptr<DB::DBBase> dbSqlLie = var.GetConnection(DB::DBType::Sqllite);
+
 
     return 0;
 
