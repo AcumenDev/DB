6f4364717cf4aa75da725698a785a7fdf2fc0ca9
Argutin Sergey <argutin.sergey@gmail.com>
2013-06-08 21:28:41 +0400

    gencode separated

--- src/CodeGeneration/Cpp/CGCpp.cpp
+++ src/CodeGeneration/Cpp/CGCpp.cpp
@@ -9,31 +9,27 @@ CGCpp::~CGCpp() {
 }
 
 void CGCpp::GenerateTables() {
-    Tools::FileSystem::DirCreate(_Setting->GetOutputDir(), "Folder");
-    std::string content="";
+    std::string content;
     for(const auto& table : _dbModel.DBTableList) {
+        Tools::FileSystem::DirCreate(_Setting->GetOutputDir(), table.TableName);
+        content="#ifndef "+table.TableName +"_H\n#define "+table.TableName+"_H\n";
         content+="class "+table.TableName +" {\n";
-        for (const auto& field : table.DBTableColumnList)
-        {
-            switch (field.ColumnType)
-            {
-            case DB::DataType::Number:
-                {
-                    content+="int ";
-                    break;
-                }
-            case DB::DataType::Text:
-                {
-                    content+="std::string ";
-                    break;
-                }
+        for (const auto& field : table.DBTableColumnList) {
+            switch (field.ColumnType) {
+            case DB::DataType::Number: {
+                content+="int ";
+                break;
+            }
+            case DB::DataType::Text: {
+                content+="std::string ";
+                break;
+            }
             }
             content+= field.ColumnName + ";\n";
         }
         content+="}\n";
-        }
-
-    Tools::FileSystem::FileSave("./DB/Folder/", "tables.h", content);
+        Tools::FileSystem::FileSave("./DB/"+table.TableName+"/", table.TableName+".h", content);
+    }
 }
 
 
