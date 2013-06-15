496c7b3728c1547e4c8193418977063c92459c15
Vladimir Stalmakov <akummail@gmail.com>
2013-06-15 11:54:51 +0400

    fix error

--- Test/DataAccess/main.cpp
+++ Test/DataAccess/main.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 int main() {
-    DBEntityTestDB dbEntity("../../122.db","");
+    DBEntityTestDB dbEntity("../../TestDB.db","");
 
     for(const auto& row :dbEntity.Test1.GetList() ) {
         std::cout<<row.Id<<" "<<row.Name<<std::endl;
     }
+
+
     std::vector<Test1> vectorTest;
     vectorTest.push_back(Test1 {5,"124"});
-
     dbEntity.Test1.InsertList(vectorTest);
 
 
