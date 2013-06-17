9311de546a84ed3db46f571d05cb20dfc5daaf83
Argutin Sergey <argutin.sergey@gmail.com>
2013-06-17 11:24:08 +0400

    fixed merge

--- Test/TestDB/main.cpp
+++ Test/TestDB/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include "../../OutputDB/DBEntityTestDB.h"
-us
+#include "DBEntityTestDB.h"
 
 using namespace std;
 
 int main() {
+    DBEntityTestDB dbEntity("../../TestDB.db");
+        for(const auto& row :dbEntity.test1.GetList() ) {
+        std::cout<<row.id<<" "<<row.name<<std::endl;
+    }
     return 0;
 }
