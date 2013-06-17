872e7464c56a9d0461ccbaf1011cedb334a60d92
Vladimir Stalmakov <akummail@gmail.com>
2013-06-17 14:43:21 +0400

    added test added tests for methods getconnection and connect

--- Test/TestDB/main.cpp
+++ Test/TestDB/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include "gtest/gtest.h"
+#include "Connection/DBConnection.h"
 
-using namespace std;
-
-int main()
+namespace
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
+using namespace Connection;
+using namespace DB;
+using namespace Core;
+TEST(Connection, GetConnection) {
+    std::shared_ptr<DBBase> dbSqlLite = DBConnection::GetConnection(DBType::Sqllite);
+    EXPECT_NE(nullptr,dbSqlLite.get());
+}
+
+
+
+TEST(Connection, Connect) {
+    std::shared_ptr<DBBase> dbSqlLite = DBConnection::GetConnection(DBType::Sqllite);
+    ASSERT_NO_THROW(dbSqlLite->Connect("../../TestDB.db","",""));
+}
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
