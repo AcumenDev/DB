4a932cd4a2ccc2f5d12953113c83c8a4ed50df57
Argutin Sergey <argutin.sergey@gmail.com>
2013-06-17 14:46:41 +0400

    still getting params

--- src/Core/Parameters.cpp
+++ src/Core/Parameters.cpp
@@ -5,58 +5,83 @@ namespace Core
 
 Parameters::Parameters(int argc, char *argv[])
 {
+
+std::string dbType;
+std::string pathToDB;
+std::string langType;
+std::string pathToOutputFiles;
+
     for (int x=1; x<argc; x++)
     {
-        std::cout<<argv[x];
+        std::string val(argv[x]);
 
+        if (val=="-dbType")
+        {
+
+        }
+
+        if (val=="-pathToDb")
+        {
+
+        }
+
+        if (val=="-langType")
+        {
+
+
+        }
+
+        if (val=="-pathToOutputFiles")
+        {
+
+
+        }
+
+        std::cout<<"a";
     }
+}
 
 
-//
-//    char* pathToOutputFiles=argv[1];
-//    char* pathToDb=argv[2];
-//    char* dbType=argv[3];
-//    char* langType=argv[4];
 }
 
 std::string Parameters::GetPathToDB() const
 {
-return _PathToDB;
+    return _PathToDB;
 }
 
 std::string Parameters::GetOutputDirPath() const
 {
-return _OutputDirPath;
+    return _OutputDirPath;
 }
 
 Core::DBType Parameters::GetDBType() const
 {
-return _DBType;
+    return _DBType;
 }
 
 Language Parameters::GetLanguage() const
 {
-return _Language;
+    return _Language;
 }
 
 void Parameters::SetPathToDB(std::string path)
 {
-_PathToDB=path;
+    _PathToDB=path;
 }
 
 void Parameters::SetOutputPath(std::string path)
 {
-_OutputDirPath=path;
+    _OutputDirPath=path;
 }
 
 void Parameters::SetDBType(Core::DBType type)
 {
-_DBType=type;
+    _DBType=type;
 }
 
 void Parameters::SetLanguage(Core::Language type)
 {
-_Language=type;
+    _Language=type;
 }
 
 Parameters::~Parameters()
