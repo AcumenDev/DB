cd2e811c11a66834af37e44984f411f5e864038b
Vladimir Stalmakov <akummail@gmail.com>
2013-06-19 14:40:40 +0400

    fexed constructors

--- src/Connection/DBBase.cpp
+++ src/Connection/DBBase.cpp
@@ -1,6 +1,5 @@
 #include "./DBBase.h"
 namespace DB {
-DBBase::DBBase() {
-    _Log =  Tools::LoggingSystem::GetLoggingSystem();
+DBBase::DBBase():_Log(Tools::LoggingSystem::GetLoggingSystem()) {
 }
 }//end namespace DB
